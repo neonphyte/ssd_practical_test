99f841b0fbf0ff5b519ec95a203db1165cabfa8b
Ong Zhi Kang <2301862@sit.singaporetech.edu.sg>
2025-07-25 16:09:48 +0800

    Step 6: Fix

--- eslint.config.js
+++ eslint.config.js
@@ -1,20 +1,11 @@
 import js from "@eslint/js";
 import globals from "globals";
 import pluginReact from "eslint-plugin-react";
-import pluginSecurity from "eslint-plugin-security";
 import { defineConfig } from "eslint/config";
 
+
 export default defineConfig([
-  {
-    files: ["**/*.{js,mjs,cjs,jsx}"],
-    plugins: {
-      js,
-      security: pluginSecurity,
-    },
-    extends: ["js/recommended", "plugin:security/recommended"],
-    languageOptions: {
-      globals: globals.browser,
-    },
-  },
+  { files: ["**/*.{js,mjs,cjs,jsx}"], plugins: { js }, extends: ["js/recommended"] },
+  { files: ["**/*.{js,mjs,cjs,jsx}"], languageOptions: { globals: globals.browser } },
   pluginReact.configs.flat.recommended,
 ]);
